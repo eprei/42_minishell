932f5821bfbbdda1e2a75912f65dce6d569ac9a4
Emiliano Presa custodio <epresa-c@c2r8s9.42lausanne.ch>
2022-06-17 17:34:04 +0200

    17 Jun end of day

--- src/main.c
+++ src/main.c
@@ -6,11 +6,19 @@
 /*   By: epresa-c <epresa-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 15:01:33 by Emiliano          #+#    #+#             */
-/*   Updated: 2022/06/17 16:25:27 by epresa-c         ###   ########.fr       */
+/*   Updated: 2022/06/17 17:02:05 by epresa-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	init(t_var *v, t_prompt *prompt, char **envp)
+{
+	g_exit_status = 0;
+	init_t_var_main(v);
+	init_t_prompt(prompt, envp);
+}
+
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -18,9 +26,9 @@ int	main(int argc, char **argv, char **envp)
 	t_prompt	prompt;
 
 	(void)argv;
-	g_exit_status = 0;
-	init_t_var_main(&v);
-	init_t_prompt(&prompt, envp);
+	init(&v, &prompt, envp);
+	// init_t_var_main(&v);
+	// init_t_prompt(&prompt, envp);
 	signal(SIGINT, signal_handler); // ctrl + C
 	signal(SIGQUIT, signal_handler); // ctrl + '\'
 	// CTRL+D = EOF of standard input, wich ends the proces; LEARN HOW TO HANDLE THIS
